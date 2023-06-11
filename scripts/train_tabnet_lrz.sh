c98a5c37e2597bf1c9e5d0aca915643b7abf7d90
Felix Fischer <fe.fischer@icloud.com>
2023-06-11 16:56:18 +0200

    bug fix

--- scripts/train_tabnet_lrz.sh
+++ scripts/train_tabnet_lrz.sh
@@ -14,7 +14,7 @@ DSSFS02_HOME="/dss/dssfs02/lwp-dss-0001/pn36po/pn36po-dss-0001/di93zer"
 DSSMCML_HOME="/dss/dssmcmlfs01/pn36po/pn36po-dss-0000/di93zer"
 
 
-SCRIPT="/dss/dsshome1/04/di93zer/git/cellnet/scripts/py_SCRIPTs/train_tabnet.py"
+SCRIPT="/dss/dsshome1/04/di93zer/git/cellnet/scripts/py_scripts/train_tabnet.py"
 GIT_REPO="/dss/dsshome1/04/di93zer/git/cellnet"
 ARGS="--version=version_1 --epochs=50"
 
