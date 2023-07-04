29e2f65aa801cbe043189811ffee4fad2b9f484f
Felix Fischer <fe.fischer@icloud.com>
2023-07-04 11:36:08 +0200

    added MLP model reference implementation

---
+++ scripts/py_scripts/train_mlp.py
@@ -0,0 +1,104 @@
+import argparse
+from random import uniform
+from time import sleep
+
+import torch
+from lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor, TQDMProgressBar
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.utilities.model_summary import ModelSummary
+from lightning.pytorch import seed_everything
+
+from cellnet.estimators import EstimatorCellTypeClassifier
+from utils import get_paths, get_model_checkpoint
+
+
+torch.set_float32_matmul_precision('high')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cluster', type=str)
+    parser.add_argument('--data_path', type=str, default=None)
+
+    parser.add_argument('--epochs', default=1000, type=int)
+    parser.add_argument('--batch_size', default=2048, type=int)
+    parser.add_argument('--sub_sample_frac', default=1., type=float)
+    parser.add_argument('--lr', default=0.002, type=float)
+    parser.add_argument('--weight_decay', default=0.05, type=float)
+    parser.add_argument('--hidden_size', default=128, type=int)
+    parser.add_argument('--n_hidden', default=8, type=int)
+    parser.add_argument('--dropout', default=0.1, type=float)
+    parser.add_argument('--augment_training_data', default=True, type=lambda x: x.lower() in ['true', '1', '1.'])
+    parser.add_argument('--lr_scheduler_step_size', default=1, type=int)
+    parser.add_argument('--lr_scheduler_gamma', default=0.9, type=float)
+    parser.add_argument('--version', default=None, type=str)
+
+    parser.add_argument('--resume_from_checkpoint', type=str, default=None)
+    parser.add_argument('--checkpoint_interval', default=1, type=int)
+    parser.add_argument('--check_val_every_n_epoch', default=1, type=int)
+
+    parser.add_argument('--seed', default=1, type=int)
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    print(args)
+
+    # config parameters
+    MODEL = 'cxg_2023_05_15_mlp'
+    CHECKPOINT_PATH, LOGS_PATH, DATA_PATH = get_paths(args.cluster, MODEL)
+    if args.data_path is not None:
+        DATA_PATH = args.data_path
+
+    sleep(uniform(0., 30.))  # add random sleep interval to avoid duplicated tensorboard log dirs
+    estim = EstimatorCellTypeClassifier(DATA_PATH)
+    seed_everything(args.seed)
+    estim.init_datamodule(batch_size=args.batch_size, sub_sample_frac=args.sub_sample_frac)
+    estim.init_trainer(
+        trainer_kwargs={
+            'max_epochs': args.epochs,
+            'gradient_clip_val': 1.,
+            'gradient_clip_algorithm': 'norm',
+            'accelerator': 'gpu',
+            'devices': 1,
+            'num_sanity_val_steps': 0,
+            'check_val_every_n_epoch': args.check_val_every_n_epoch,
+            'logger': [TensorBoardLogger(LOGS_PATH, name='default', version=args.version)],
+            'log_every_n_steps': 200,
+            'detect_anomaly': False,
+            'enable_progress_bar': True,
+            'enable_model_summary': False,
+            'enable_checkpointing': True,
+            'default_root_dir': CHECKPOINT_PATH,
+            'callbacks': [
+                TQDMProgressBar(refresh_rate=250),
+                LearningRateMonitor(logging_interval='step'),
+                ModelCheckpoint(filename='last_{epoch}', every_n_epochs=args.checkpoint_interval),
+                ModelCheckpoint(filename='val_f1_macro_{epoch}_{val_f1_macro:.3f}', monitor='val_f1_macro', mode='max',
+                                every_n_epochs=args.checkpoint_interval, save_top_k=2),
+                ModelCheckpoint(filename='val_loss_{epoch}_{val_loss:.3f}', monitor='val_loss', mode='min',
+                                every_n_epochs=args.checkpoint_interval, save_top_k=2)
+            ],
+        }
+    )
+    estim.init_model(
+        model_type='mlp',
+        model_kwargs={
+            'learning_rate': args.lr,
+            'weight_decay': args.weight_decay,
+            'lr_scheduler': torch.optim.lr_scheduler.StepLR,
+            'lr_scheduler_kwargs': {
+                'step_size': args.lr_scheduler_step_size,
+                'gamma': args.lr_scheduler_gamma,
+                'verbose': True
+            },
+            'optimizer': torch.optim.AdamW,
+            'hidden_size': args.hidden_size,
+            'n_hidden': args.n_hidden,
+            'augment_training_data': args.augment_training_data
+        },
+    )
+    print(ModelSummary(estim.model))
+    estim.train(ckpt_path=get_model_checkpoint(CHECKPOINT_PATH, args.resume_from_checkpoint))
---
+++ scripts/train_mlp_lrz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH -J mlp
+#SBATCH --output=slurm_out/mlp_out.%j
+#SBATCH --error=slurm_out/mlp_err.%j
+#SBATCH --partition=mcml-dgx-a100-40x8
+#SBATCH --qos mcml
+#SBATCH --gres=gpu:1
+#SBATCH --time 3-00:00:00
+#SBATCH --mem=90GB
+#SBATCH --cpus-per-task=6
+
+DSSFS02_HOME="/dss/dssfs02/lwp-dss-0001/pn36po/pn36po-dss-0001/di93zer"
+DSSMCML_HOME="/dss/dssmcmlfs01/pn36po/pn36po-dss-0000/di93zer"
+
+
+SCRIPT="/dss/dsshome1/04/di93zer/git/cellnet/scripts/py_scripts/train_mlp.py"
+GIT_REPO="/dss/dsshome1/04/di93zer/git/cellnet"
+ARGS="--version=version_1 --epochs=48"
+
+srun --cpu-bind=verbose,socket --accel-bind=g --gres=gpu:1 \
+     --container-mounts="/dss:/dss,${DSSFS02_HOME}:/mnt/dssfs02,${DSSMCML_HOME}:/mnt/dssmcmlfs01" \
+     --container-image="/dss/dsshome1/04/di93zer/merlin-2302.sqsh" \
+     --no-container-remap-root \
+     bash -c "pip install -e ${GIT_REPO} --no-deps && python -u ${SCRIPT} --cluster=lrz ${ARGS}"
